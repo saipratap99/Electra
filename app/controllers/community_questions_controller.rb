b464b23f4cc553eb5c6a1ed825fa289482ba0399
saipratap99 <122005067@sastra.ac.in>
2020-08-30 17:12:06 +0530

    Getting questions and answers for a question BASIC done!

--- app/controllers/community_questions_controller.rb
+++ app/controllers/community_questions_controller.rb
@@ -1,7 +1,10 @@
 class CommunityQuestionsController < ApplicationController
   def index
+    @questions = CommunityQuestion.all.order(id: :desc)
   end
 
   def show
+    id = params[:id]
+    @question = CommunityQuestion.find(id)
   end
 end
