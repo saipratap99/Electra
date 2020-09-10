bf2f71a9c78e7c7cec0a8a43a281c4e6fb780d6b
saipratap99 <122005067@sastra.ac.in>
2020-09-10 17:06:20 +0530

    User can answer to the question --BASIC

--- app/controllers/community_answers_controller.rb
+++ app/controllers/community_answers_controller.rb
@@ -1,2 +1,7 @@
 class CommunityAnswersController < ApplicationController
+  def create
+    @ques = CommunityQuestion.find(params[:id])
+    @ques.community_answers.create(answer: params[:answer])
+    redirect_to @ques
+  end
 end
