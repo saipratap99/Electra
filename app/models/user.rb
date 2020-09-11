da0240d8aad34cd2083c9e2bdd343ca91c392a4b
saipratap99 <122005067@sastra.ac.in>
2020-09-11 07:07:42 +0530

    Devise gem is working now!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :registerable, :confirmable, :trackable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :trackable
 end
